c02aa1a281f0564e31e5316098320e709b192d18
Philip Mosquera Prieto <philmosquera@gmail.com>
2014-11-10 19:33:31 +0000

    Bitboard now generates properly with a1/(0,0) being the right most bit and h8/(7,7) being the left most bit

--- src/test/ChessBoard.java
+++ src/test/ChessBoard.java
@@ -1,7 +1,5 @@
 package test;
 
-import java.util.function.BinaryOperator;
-
 public class ChessBoard {
 
 	public static void main(String[] args) {
@@ -10,9 +8,9 @@ public class ChessBoard {
 	}
 
 	private long whitePawns = 0L, whiteRooks = 0L, whiteKnights = 0L,
-			whiteBishops = 0L, whiteQueen = 0L, whiteKing = 0L;
+			whiteBishops = 0L, whiteQueens = 0L, whiteKing = 0L;
 	private long blackPawns = 0L, blackRooks = 0L, blackKnights = 0L,
-			blackBishops = 0L, blackQueen = 0L, blackKing = 0L;
+			blackBishops = 0L, blackQueens = 0L, blackKing = 0L;
 	// Upper case for WHITE
 	// Lower case for BLACK
 	// 0,0 is top left 0,7 is top right 7,7 bottom right
@@ -27,20 +25,70 @@ public class ChessBoard {
 			{ "R", "P", " ", " ", " ", " ", "p", "r" } };;
 
 	public ChessBoard() {
-
 		initialiseBoard();
-
-		// printBoard();
+		System.out.println(Long.toBinaryString(blackRooks).length());
 	}
 
 	private void initialiseBoard() {
-
 		String binaryLong;
-		for (int x = 0; x < 8; x++) {
-			for (int y = 0; y < 8; y++) {
+		int currentIndex;
+		for (int y = 7; y >= 0; y--) {
+			for (int x = 0; x < 8; x++) {
+				// 64 bits and currentIndex is the square being looked at
 				binaryLong = "0000000000000000000000000000000000000000000000000000000000000000";
-
+				currentIndex = (y * 8) + x;
+				binaryLong = binaryLong.substring(currentIndex,
+						binaryLong.length() - 1)
+						+ "1" + binaryLong.substring(0, currentIndex);
+				switch (currentBoard[x][y]) {
+				case "P":
+					whitePawns += convertStringToBinary(binaryLong);
+					break;
+				case "R":
+					whiteRooks += convertStringToBinary(binaryLong);
+					break;
+				case "N":
+					whiteKnights += convertStringToBinary(binaryLong);
+					break;
+				case "B":
+					whiteBishops += convertStringToBinary(binaryLong);
+					break;
+				case "Q":
+					whiteQueens += convertStringToBinary(binaryLong);
+					break;
+				case "K":
+					whiteKing += convertStringToBinary(binaryLong);
+					break;
+				case "p":
+					blackPawns += convertStringToBinary(binaryLong);
+					break;
+				case "r":
+					blackRooks += convertStringToBinary(binaryLong);
+					break;
+				case "n":
+					blackKnights += convertStringToBinary(binaryLong);
+					break;
+				case "b":
+					blackBishops += convertStringToBinary(binaryLong);
+					break;
+				case "q":
+					blackQueens += convertStringToBinary(binaryLong);
+					break;
+				case "k":
+					blackKing += convertStringToBinary(binaryLong);
+					break;
+				default:
+					break;
+				}
 			}
+		}
+	}
+
+	private long convertStringToBinary(String binaryLong) {
+		if (binaryLong.charAt(0) == '0') {
+			return Long.parseLong(binaryLong, 2);
+		} else {
+			return Long.parseLong("1" + binaryLong.substring(2), 2) * 2;
 		}
 	}
 
@@ -49,7 +97,6 @@ public class ChessBoard {
 	 * relying on a gui.
 	 */
 	public void printBoard() {
-
 		System.out.println();
 		for (int y = 7; y >= 0; y--) {
 			for (int x = 0; x < currentBoard.length; x++) {
@@ -62,5 +109,19 @@ public class ChessBoard {
 			}
 			System.out.println();
 		}
+	}
+
+	private long returnWhitePieces() {
+		return (whiteBishops | whiteKing | whiteKnights | whitePawns
+				| whiteQueens | whiteRooks);
+	}
+
+	private long returnBlackPieces() {
+		return (blackBishops | blackKing | blackKnights | blackPawns
+				| blackQueens | blackRooks);
+	}
+
+	private long returnAllPieces() {
+		return (returnWhitePieces() | returnBlackPieces());
 	}
 }

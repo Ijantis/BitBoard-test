ee1eca0683a7992982fc818f4f141fd0c8f3a41d
Philip Mosquera Prieto <philmosquera@gmail.com>
2015-03-18 17:23:08 +0000

    Added move ordering as a counter so it can be done multiple times. The random ai now plays a more random move

--- src/search/Engine.java
+++ src/search/Engine.java
@@ -77,7 +77,8 @@ public class Engine {
 	private static FullGameState calculateAlphaBeta(
 			FullGameState currentGameState, boolean whiteToMove,
 			ArrayList<FullGameState> nextDepth) {
-		int depth = 3;
+		int depth = 4;
+		int moveOrdering = 2;
 
 		if (whiteToMove) {
 			int bestIndex = 0;
@@ -87,7 +88,7 @@ public class Engine {
 			for (int i = 0; i < nextDepth.size(); i++) {
 				currentScore = alphaBeta(nextDepth.get(i), depth,
 						Integer.MIN_VALUE, Integer.MAX_VALUE, !whiteToMove,
-						true);
+						moveOrdering);
 				if (currentScore > bestValue) {
 					bestIndex = i;
 					bestValue = currentScore;
@@ -104,7 +105,7 @@ public class Engine {
 			for (int i = 0; i < nextDepth.size(); i++) {
 				currentScore = alphaBeta(nextDepth.get(i), depth,
 						Integer.MAX_VALUE, Integer.MIN_VALUE, !whiteToMove,
-						true);
+						moveOrdering);
 				if (currentScore < bestValue) {
 					bestIndex = i;
 					bestValue = currentScore;
@@ -144,16 +145,23 @@ public class Engine {
 	 * @return - The evaluation of the position to a given depth.
 	 */
 	private static long alphaBeta(FullGameState currentGameState, int depth,
-			long alpha, long beta, boolean whiteToMove, boolean shouldOrder) {
+			long alpha, long beta, boolean whiteToMove, int shouldOrder) {
 
 		if (depth == 0) {
+			// System.out.println();
+			// System.out.println();
+			// System.out.println(Evaluator.evaluatePosition(currentGameState));
+			// BitboardOperations.createArrayFromGamestate(currentGameState);
 			return Evaluator.evaluatePosition(currentGameState);
 		}
 
 		if (whiteToMove) {
-			if (shouldOrder) {
+			if (shouldOrder > 0) {
 				ArrayList<FullGameState> nextDepth = MoveGenerator
 						.generateWhiteLegalMoves(currentGameState);
+				if (nextDepth.size() == 0) {
+					return Evaluator.evaluatePosition(currentGameState);
+				}
 				TreeMap<Long, FullGameState> orderedNextDepth = new TreeMap<Long, FullGameState>();
 				// sorting the moves in order
 				for (int i = 0; i < nextDepth.size(); i++) {
@@ -168,7 +176,7 @@ public class Engine {
 							value,
 							alphaBeta(orderedNextDepth.get(orderedNextDepth
 									.lastKey()), depth - 1, alpha, beta,
-									!whiteToMove, false));
+									!whiteToMove, shouldOrder - 1));
 					alpha = Math.max(alpha, value);
 					if (alpha >= beta) {
 						break;
@@ -178,13 +186,16 @@ public class Engine {
 				// without move ordering
 			} else {
 				long value = Integer.MIN_VALUE;
-				ArrayList<FullGameState> childNodes = MoveGenerator
+				ArrayList<FullGameState> nextDepth = MoveGenerator
 						.generateWhiteLegalMoves(currentGameState);
-				for (int i = 0; i < childNodes.size(); i++) {
+				if (nextDepth.size() == 0) {
+					return Evaluator.evaluatePosition(currentGameState);
+				}
+				for (int i = 0; i < nextDepth.size(); i++) {
 					value = Math.max(
 							value,
-							alphaBeta(childNodes.get(i), depth - 1, alpha,
-									beta, !whiteToMove, false));
+							alphaBeta(nextDepth.get(i), depth - 1, alpha, beta,
+									!whiteToMove, 0));
 					alpha = Math.max(alpha, value);
 					if (alpha >= beta) {
 						break;
@@ -195,9 +206,12 @@ public class Engine {
 		}
 
 		else {
-			if (shouldOrder) {
+			if (shouldOrder > 0) {
 				ArrayList<FullGameState> nextDepth = MoveGenerator
 						.generateBlackLegalMoves(currentGameState);
+				if (nextDepth.size() == 0) {
+					return Evaluator.evaluatePosition(currentGameState);
+				}
 				TreeMap<Long, FullGameState> orderedNextDepth = new TreeMap<Long, FullGameState>();
 				// sorting the moves in order
 				for (int i = 0; i < nextDepth.size(); i++) {
@@ -212,7 +226,7 @@ public class Engine {
 							value,
 							alphaBeta(orderedNextDepth.get(orderedNextDepth
 									.firstKey()), depth - 1, alpha, beta,
-									!whiteToMove, false));
+									!whiteToMove, shouldOrder - 1));
 					orderedNextDepth.remove(orderedNextDepth.firstKey());
 					beta = Math.min(beta, value);
 					if (alpha >= beta) {
@@ -223,13 +237,16 @@ public class Engine {
 			} else {
 
 				long value = Integer.MAX_VALUE;
-				ArrayList<FullGameState> childNodes = MoveGenerator
+				ArrayList<FullGameState> nextDepth = MoveGenerator
 						.generateBlackLegalMoves(currentGameState);
-				for (int i = 0; i < childNodes.size(); i++) {
+				if (nextDepth.size() == 0) {
+					return Evaluator.evaluatePosition(currentGameState);
+				}
+				for (int i = 0; i < nextDepth.size(); i++) {
 					value = Math.min(
 							value,
-							alphaBeta(childNodes.get(i), depth - 1, alpha,
-									beta, !whiteToMove, false));
+							alphaBeta(nextDepth.get(i), depth - 1, alpha, beta,
+									!whiteToMove, 0));
 					beta = Math.min(beta, value);
 					if (alpha >= beta) {
 						break;
@@ -319,7 +336,7 @@ public class Engine {
 			listOfMoves = generateBlackLegalMoves(gamestate);
 		}
 
-		Random myRandom = new Random();
+		Random myRandom = new Random(System.currentTimeMillis());
 		return listOfMoves.get(myRandom.nextInt(listOfMoves.size()));
 
 	}
